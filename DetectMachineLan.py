f49972be39a23beab42945de05771933772720f7
sagitariozod <delfinb@gmail.com>
2017-04-24 02:13:29 +0200

    Update DetectMachineLan.py

--- DetectMachineLan.py
+++ DetectMachineLan.py
@@ -1,41 +1,52 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
 """ Detect machine LAN """
 
 import sys
 import nmap
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import Ether, ARP, srp
 import re
 import os
 import smtplib
 import time,datetime
-import gtk
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+gi.require_version('Notify', '0.7')
+from gi.repository import Gtk
+from gi.repository import AppIndicator3
+from gi.repository import Notify
 from optparse import OptionError
 from optparse import OptionGroup
 from optparse import OptionParser
+import signal
 
-__author__ 		= "GoldraK"
-__credits__ 	= "GoldraK"
-__version__ 	= "0.1"
-__maintainer__ 	= "GoldraK"
-__email__ 		= "goldrak@gmail.com"
-__status__ 		= "Development"
+__author__	= "GoldraK"
+__credits__	= "GoldraK"
+__version__	= "0.1.1"
+__maintainer__	= "Sagitariozod"
+__email__	= "delfinb@gmail.com"
+__status__	= "Development"
+__adaptedby__	= "Sagitariozod"
 
+APPINDICATOR_ID = "appindicator"
 
 class DetectMachineLan():
 
 	def __init__(self):
-		self.version = "0.1"
+		self.version = "0.1.1"
 		self.whitelist_file = ""
+		self.log_file = ""
 		self.log = False
 		self.verbose = False
-
-
 
 	def DetectMachineLan(self):
 		(opts, args) = self.__handleArguments()
 		if opts.macsearch and opts.ip:
-			self.__detectMachinesNetwork(opts.ip)
+			self.__detectMachinesNetwork(opts)
 		if opts.macadd:
 			macs = opts.macadd.split(",")
 			for x in macs:
@@ -44,68 +55,190 @@ class DetectMachineLan():
 			macs = opts.macremove.split(",")
 			for x in macs:
 				self.__removeWhitelist(x)
+		if opts.ip and opts.xnotify:
+			signal.signal(signal.SIGINT, signal.SIG_DFL)
+			self.mainGtk(opts)
 		if opts.ip and opts.macsearch == False:
 			self.__detectMachinesWhitelist(opts)
 
+	def mainGtk(self,opts):
+		indicator = AppIndicator3.Indicator.new(
+												APPINDICATOR_ID,
+												os.path.dirname(os.path.abspath(__file__))+"/Detect_Network-GTK.png",
+												AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
+		indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+		indicator.set_menu(self.menu_control(opts))
+		Notify.init(APPINDICATOR_ID)
 
-	def __scanNetwork(self,ip):
-		nm = nmap.PortScanner()
-		machines=nm.scan(hosts=ip, arguments='-sP') 
-		return machines
+		Gtk.main()
+		sys.exit(0)
 
-	def __detectMachinesNetwork(self,ip):
-		machines = self.__scanNetwork(ip)
-		for k,v in machines['scan'].iteritems(): 
-			if str(v['status']['state']) == 'up':
-				print "-------"
-				try:   
-					print str(v['addresses']['ipv4'])+" --> "+str(v['addresses']['mac'])
-				except: 
-					print str(v['addresses']['ipv4'])+" --> Mac no detected"
-
-	def __detectMachinesWhitelist(self,opts):
+	def menu_control(self,opts):
+		"""Return a Gtk+ menu."""
+		menu = Gtk.Menu()
 		whitelist = self.__read_file()
+		msg = ''
+		alert_mac = ''
+		check_mac = []
+		if self.log:
+			self.__writeLog(msg)
+		if opts.scapy:
+			machines = self.__scanNetworkScapy(opts.ip)
+		else:
+			machines = self.__scanNetworkNmap(opts.ip)
+	
+		for ip,mac in machines:
+			try:
+				msg = ''
+				if mac in whitelist and mac in check_mac:
+					msg = 'Mac find: '+mac+'\tIp: '+ip+'\tWARNING Mac duplicate'
+					alert_mac = alert_mac+'Mac find: '+mac+'\tIp: '+ip+'\tWARNING Mac duplicate\n'
+					item_menu = Gtk.MenuItem(msg)
+					item_menu.connect('activate', self.menu_action)
+					menu.append(item_menu)
+				elif mac in whitelist and mac not in check_mac:
+					msg = 'Mac find: '+mac+'\tIp: '+ip
+				elif mac not in whitelist and mac in check_mac:
+					msg = 'New mac detected: '+mac+'\tIp: '+ip+'\tWARNING Mac duplicate'
+					alert_mac = alert_mac+'New mac detected: '+mac+'\tIp: '+ip+'\tWARNING Mac duplicate\n'
+					item_menu = Gtk.MenuItem(msg)
+					item_menu.connect('activate', self.menu_action)
+					menu.append(item_menu)
+				else:
+					msg = 'New mac detected: '+mac+'\tIp: '+ip
+					alert_mac = alert_mac+'New mac detected: '+mac+'\tIp: '+ip+'\n'
+					item_menu = Gtk.MenuItem(msg)
+					item_menu.connect('activate', self.menu_action)
+					menu.append(item_menu)
+				check_mac.append(mac)
+				if self.verbose:
+					self.__consoleMessage(msg)
+				if self.log:
+					self.__writeLog(msg)
+			except: 
+				if self.verbose:
+					self.__consoleMessage(msg)						
+				if self.log:
+					self.__writeLog(msg)
+				self.__gtkinfo("Errors occurred in run.")
 
-		alert_mac = ""
+		item_menu = Gtk.SeparatorMenuItem()
+		menu.append(item_menu)
+	
+		item_quit = Gtk.MenuItem("Quit")
+		item_quit.connect('activate', self.quit)
+		menu.append(item_quit)
 
-		machines = self.__scanNetwork(opts.ip)
-
-		for k,v in machines['scan'].iteritems(): 
-			if str(v['status']['state']) == 'up':
-				#print str(v)
-				try:   
-					if str(v['addresses']['mac']) in whitelist:
-						msg = 'Mac find '+str(v['addresses']['mac'])+' Ip: '+str(v['addresses']['ipv4'])
-						if self.verbose:
-							self.__consoleMessage(msg)
-						if self.log:
-							self.__writeLog(msg)
-					else:
-						alert_mac = alert_mac+'New mac detected '+str(v['addresses']['mac'])+' Ip: '+str(v['addresses']['ipv4'])+'\n'
-						msg = 'New mac detected '+str(v['addresses']['mac'])+' Ip: '+str(v['addresses']['ipv4'])
-						if self.verbose:
-							self.__consoleMessage(msg)						
-						if self.log:
-							self.__writeLog(msg)
-
-				except: 
-					msg = 'Mac not detected '+str(v['addresses']['ipv4'])
-					if self.verbose:
-						self.__consoleMessage(msg)						
-					if self.log:
-						self.__writeLog(msg)
+		menu.show_all()
+		
 		if opts.emailto:
 			self.__sendEmail(alert_mac,opts)
 		if opts.gtk:
 			self.__gtkinfo(alert_mac)
 
+		Notify.init("DetectMachineLan")
+		Notify.Notification.new('Ip and Mac found:', alert_mac,	None).show()
+	
+		return menu
 
+	def menu_action(self,source):
+		Notify.Notification.new(source.get_label(), source.get_label(),	None).show()
+
+	def quit(self,source):
+		Notify.uninit()
+		Gtk.main_quit()
+
+	def __scanNetworkNmap(self,ip):
+		nm = nmap.PortScanner()
+		machines_nmap=nm.scan(hosts=ip, arguments='-sP')
+		machines = []
+		for k,v in machines_nmap['scan'].items():
+			if str(v['status']['state']) == 'up':
+				try:
+					machines.append([str(v['addresses']['ipv4']),str(v['addresses']['mac'])])
+				except:
+					machines.append([str(v['addresses']['ipv4']),'00:00:00:00:00:00']) #Mac not detected
+		return sorted(machines,key=lambda machines:machines[1])
+
+	def __scanNetworkScapy(self,ip):
+		"""Arping function takes IP Address or Network, returns nested ip/mac list"""
+		machines = []
+		try:
+			ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2, verbose=False)
+			for snd, rcv in ans:
+				result = ((rcv.sprintf(r"%ARP.psrc% %Ether.src%")).upper().split())
+				machines.append(result)
+		except:
+			pass
+		return sorted(machines,key=lambda machines:machines[1])
+
+	def __detectMachinesNetwork(self,opts):
+		check_mac = []
+		if opts.scapy:
+			machines = self.__scanNetworkScapy(opts.ip)
+		else:
+			machines = self.__scanNetworkNmap(opts.ip)
+		for ip,mac in machines:
+			print("")
+			try:
+				if mac in check_mac:
+					print(ip+" --> "+mac+ "\tWARNING Mac duplicate")
+				else:
+					print(ip+" --> "+mac)
+				check_mac.append(mac)
+			except:
+				pass
+		print("")
+
+	def __detectMachinesWhitelist(self,opts):
+		whitelist = self.__read_file()
+		msg = ""
+		alert_mac = ""
+		check_mac = []
+		if self.log:
+			self.__writeLog(msg)
+		if opts.scapy:
+			machines = self.__scanNetworkScapy(opts.ip)
+		else:
+			machines = self.__scanNetworkNmap(opts.ip)
+		for ip,mac in machines:
+			try:
+				if mac in whitelist:
+					if mac in check_mac:
+						msg = 'Mac find: '+mac+'  Ip: '+ip+'\tWARNING Mac duplicate'
+						alert_mac = alert_mac+'Mac find: '+mac+'  Ip: '+ip+'\tWARNING Mac duplicate\n'
+					else:
+						msg = 'Mac find: '+mac+' Ip: '+ip
+					if self.verbose:
+						self.__consoleMessage(msg)
+					if self.log:
+						self.__writeLog(msg)
+				else:
+					if mac in check_mac:
+						msg = 'New mac detected: '+mac+'  Ip: '+ip+'\tWARNING Mac duplicate'
+						alert_mac = alert_mac+'New mac detected '+mac+'  Ip: '+ip+'\tWARNING Mac duplicate\n'
+					else:
+						msg = 'New mac detected: '+mac+'  Ip: '+ip
+						alert_mac = alert_mac+'New mac detected: '+mac+'  Ip: '+ip+'\n'
+					if self.verbose:
+						self.__consoleMessage(msg)
+					if self.log:
+						self.__writeLog(msg)
+				check_mac.append(mac)
+			except: 
+				if self.verbose:
+					self.__consoleMessage(msg)						
+				if self.log:
+					self.__writeLog(msg)
+		if opts.emailto:
+			self.__sendEmail(alert_mac,opts)
+		if opts.gtk:
+			self.__gtkinfo(alert_mac)
 
 	def __handleArguments(self,argv=None):
 		"""
 		This function parses the command line parameters and arguments
 		"""
-
 		parser = OptionParser()
 		if not argv:
 			argv = sys.argv
@@ -118,22 +251,22 @@ class DetectMachineLan():
 		mac.add_option('--mr','--macremove', action='store', dest='macremove', help='Remove mac from whitelist')
 
 
-		email = OptionGroup(parser, "Email", "You need user,password,server and destination"
+		email = OptionGroup(parser, "Email", "You need user, password, server and destination "
 			"options has to be provided to define the server send mail")
 
 		email.add_option('-u','--user', action='store', dest='user', help='User mail server')
 		email.add_option('--pwd','--password', action='store', dest='password', help='Password mail server')
-		email.add_option('-s','--server', action='store', dest='server', help='mail server')
+		email.add_option('-s','--server', action='store', dest='server', help='Mail server')
 		email.add_option('-p','--port', action='store', default='25', dest='port', help='Port mail server')
 		email.add_option('--et','--emailto', action='store', dest='emailto', help='Destination E-mail')
 
-
 		parser.add_option('-r','--range', action='store', dest='ip', help='Secure network range ')
-		parser.add_option('--wl','--whitelist', action='store', default='whitelist.txt' , dest='whitelist_file', help='File have Mac whitelist ')
+		parser.add_option('--wl','--whitelist', action='store', default=os.path.dirname(os.path.abspath(__file__))+'/whitelist.txt' , dest='whitelist_file', help='File have Mac whitelist ')
 		parser.add_option('-l','--log', action='store_true', default=False, dest='log', help='Log acctions script')
-		parser.add_option('-v','--verbose', action='store_true', default=False, dest='verbose', help='Verbose acctions script')
+		parser.add_option('-v','--verbose', action='store_true', default=False, dest='verbose', help='Verbose actions script')
 		parser.add_option('-g','--gui', action='store_true', default=False, dest='gtk', help='GTK Windows with info')
-
+		parser.add_option('--ns','--scapy', action='store_true', default=False, dest='scapy', help='Scan network whith Scapy')
+		parser.add_option('-x','--xnotify', action='store_true', default=False, dest='xnotify', help='GTK Notify info with icon')
 
 		parser.add_option_group(mac)
 		parser.add_option_group(email)
@@ -141,6 +274,7 @@ class DetectMachineLan():
 		(opts, args) = parser.parse_args()
 
 		self.log = opts.log
+		self.log_file = os.path.dirname(os.path.abspath(__file__))+'/log.txt'
 		self.verbose = opts.verbose
 		self.whitelist_file = opts.whitelist_file
 
@@ -166,9 +300,9 @@ class DetectMachineLan():
 		header += 'To: %s\n' % opts.emailto
 		if alert_mac:
 			header += 'Subject: New machines connected\n\n'
-			message = header + 'List macs: \n '+str(alert_mac)
+			message = header + 'List macs: \n'+str(alert_mac)
 		else:
-			header += 'Subject: No intruders - All machines known \n\n'
+			header += 'Subject: No intruders - All machines are known \n\n'
 			message = header + 'No intruders'
 
 		server = smtplib.SMTP(opts.server+":"+opts.port)
@@ -177,29 +311,37 @@ class DetectMachineLan():
 		if self.verbose or self.log:
 			debugemail = server.set_debuglevel(1)
 			if self.verbose:
-				self.__consoleMessage(debugemail)
+				self.__consoleMessage('debugemail')
 		problems = server.sendmail(opts.user, opts.emailto, message)
-		print problems
+		if self.verbose:
+			print(problems)
 		server.quit()
 
 	def __gtkinfo(self,alert_mac):
-		parent = None
+		parent = Gtk.Window()
 		if alert_mac:
-			md = gtk.MessageDialog(parent, 
-				gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_WARNING, 
-				gtk.BUTTONS_CLOSE, 'List macs: \n '+str(alert_mac))
+			md = Gtk.Dialog('List macs:', parent, 
+				Gtk.DialogFlags.DESTROY_WITH_PARENT | Gtk.DialogFlags.MODAL,
+				(Gtk.STOCK_CLOSE, Gtk.ButtonsType.CLOSE))
+			label = Gtk.Label(str(alert_mac))
 		else:
-			md = gtk.MessageDialog(parent, 
-				gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, 
-				gtk.BUTTONS_CLOSE, "No intruders - All machines known")
-		md.run()
+			md = Gtk.Dialog('No intruders:', parent, 
+				Gtk.DialogFlags.DESTROY_WITH_PARENT | Gtk.DialogFlags.MODAL,
+				(Gtk.STOCK_OK, Gtk.ButtonsType.OK))
+			label = Gtk.Label("\nAll machines are known.\n")
 
+		md.set_default_size(500, 0)
+		box = md.get_content_area()
+		box.add(label)
+		md.show_all()
+		md.run()
+		md.destroy()
+		parent = None
 
 	def __consoleMessage(self,message):
 		ts = time.time()
 		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		print '['+st+'] '+str(message)
-
+		print('['+st+'] '+str(message))
 
 	def __writeLog(self,log):
 		"""
@@ -207,18 +349,21 @@ class DetectMachineLan():
 		"""
 		ts = time.time()
 		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		if os.path.isfile('log.txt'):
+		if os.path.isfile(self.log_file):
 			try:
-				file_read = open('log.txt', 'a')
-				file_read.write('['+st+'] '+log+"\n")
+				file_read = open(self.log_file, 'a')
+				if log:
+					file_read.write('['+st+'] '+log+"\n")
+				else:
+					file_read.write(log+"\n")
 				file_read.close()
 			except IOError:
-				msg = 'ERROR: Cannot open'+ self.whitelist_file
+				msg = 'ERROR: Cannot open'+ self.log_file
 				if self.verbose:
 					self.__consoleMessage(msg)
 				sys.exit(-1)
 		else:
-			msg = "ERROR: The Whitelist file ", self.whitelist_file, " doesn't exist!"
+			msg = "ERROR: The Log file ", self.log_file, " doesn't exist!"
 			if self.verbose:
 				self.__consoleMessage(msg)
 			sys.exit(-1)
@@ -240,7 +385,7 @@ class DetectMachineLan():
 					if self.log:
 						self.__writeLog(msg) 
 				except IOError:
-					print 
+					print()
 					msg = 'ERROR: Cannot open'+ self.whitelist_file
 					if self.verbose:
 						self.__consoleMessage(msg)
@@ -328,7 +473,6 @@ class DetectMachineLan():
 			if self.log:
 				self.__writeLog(msg) 
 			sys.exit(-1)
-
 
 
 
